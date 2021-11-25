510250f73c52828c3dccba68055dc0e7afcdd10c
Gaetan Nicod <nicod.ga@gmail.com>
2021-11-25 08:35:37 +0100

    working upload to s3

--- go.mod
+++ go.mod
@@ -3,8 +3,19 @@ module github.com/gnicod/bupcket
 go 1.16
 
 require (
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/kataras/iris/v12 v12.1.8 // indirect
-	github.com/spf13/cobra v1.2.1 // indirect
-	github.com/spf13/viper v1.8.1 // indirect
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/aws/aws-sdk-go v1.42.12
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.3.0
+	github.com/imkira/go-interpol v1.1.0 // indirect
+	github.com/kataras/iris/v12 v12.1.8
+	github.com/moul/http2curl v1.0.0 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
+	github.com/spf13/cobra v1.2.1
+	github.com/spf13/viper v1.8.1
+	github.com/valyala/fasthttp v1.31.0 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0 // indirect
+	github.com/yudai/gojsondiff v1.0.0 // indirect
+	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
 )
--- storage/s3.go
+++ storage/s3.go
@@ -3,22 +3,43 @@ package storage
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 type S3Provider struct {
-
+	session *session.Session
 }
 
-func (s3 S3Provider) Upload(request UploadRequest) (string, error){
+func (s S3Provider) Upload(request UploadRequest) (string, error){
 	// TODO send file to s3
 	fmt.Println(request.Key)
 	fmt.Println("TODO upload to s3")
 	path := "path"+request.Key
 	b, _ := ioutil.ReadFile(request.Body.Name()) 
 	fmt.Println(string(b))
-	return path, nil
+	_, err := s3.New(s.session).PutObject(&s3.PutObjectInput{
+		Bucket:             aws.String(request.Bucket),
+		Key:                aws.String(request.Key),
+		ACL:                aws.String("public-read"),
+		Body:               &request.Body, // bytes.NewReader(buffer),
+		ContentDisposition: aws.String("attachment"),
+		// ContentLength:      aws.Int64(int64(len(buffer))),
+		// ContentType:        aws.String(http.DetectContentType(buffer)),
+		// ServerSideEncryption: aws.String("AES256"),
+	})
+	return path, err
 }
 
 func NewS3Provider() (*S3Provider) {
-	return &S3Provider{}
+	sess, err := session.NewSession(&aws.Config{Region: aws.String("eu-west-3")})
+	if err != nil {
+		log.Fatalf("session.NewSession, err: %v", err)
+	}
+	return &S3Provider{
+		session: sess,
+	}
 }
